59b661194e689244d50b0e16ef89c7cc529a3fd1
Christian Dersch <lupinix@mailbox.org>
2019-09-06 16:31:04 +0200

    Initial TAP support

---
+++ applausequery/dr3/lightcurve.py
@@ -0,0 +1,25 @@
+#
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+#
+
+# internal imports
+from .. import tap_session
+from ..utils import tap
+
+# Standard library imports
+
+# Third party modules
+
+__all__ = ['query_basic_lc_by_ucac4_id']
+
+def query_basic_lc_by_ucac4_id(ucac4_id, force_download=False):
+    query = "SELECT jd_mid,bmag,bmagerr,vmag,vmagerr \
+        FROM applause_dr3.lightcurve \
+        WHERE bmag IS NOT NULL \
+        AND bmagerr IS NOT NULL \
+        AND vmag IS NOT NULL \
+        AND vmagerr IS NOT NULL \
+        AND ucac4_id=\'%s\' \
+        ORDER BY jd_mid" %(ucac4_id)
+    lc = tap_session.run_async(query)
+    return lc.to_table()
---
+++ applausequery/utils/tap.py
@@ -0,0 +1,57 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+Module providing access to APPLAUSE TAP service, uses PyVO
+"""
+
+import pyvo as vo 
+
+class ApplauseTAP(vo.dal.TAPService):
+    """
+    Represents APPLAUSE TAP service
+    """
+
+    def __init__(self, token=None):
+        """
+        Instanciate APPLAUSE TAP session
+
+        Parameters
+        ----------
+        token : str, optional
+            Authentication token for personal access to APPLAUSE
+        """
+        super().__init__("https://www.plate-archive.org/tap")
+        self.__token = token
+        if token is not None:
+            vo.utils.http.session.headers['Authorization'] = 'Token ' + token
+    
+
+    def set_token(self, token):
+        """
+        Set authentication token for APPLAUSE
+
+        Parameters
+        ----------
+        token: str or None
+            Authentication token for personal access to APPLAUSE, set to None to unset token
+        """
+        self.__token = token
+        if token is not None:
+            vo.utils.http.session.headers['Authorization'] = 'Token ' + token
+        elif token is None:
+            try:
+                vo.utils.http.session.headers.pop('Authorization')
+            except KeyError:
+                pass
+    
+    def get_token(self):
+        """
+        Get current authentication token
+
+        Returns
+        -------
+        token: str
+            Authentication token for personal access to APPLAUSE, None means: no token set
+        """
+        return self.__token
+
